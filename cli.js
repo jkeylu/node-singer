c258afeac7bfbcfaa50c14656d38e1714976ccad
jKey Lu <jkeylu@gmail.com>
2014-06-17 22:35:11 +0800

    binding some keypress

--- cli.js
+++ cli.js
@@ -1,7 +1,8 @@
 var fs = require('fs')
   , path = require('path')
   , os = require('os')
-  , Singer = require('./index')
+  , keypress = require('keypress')
+  , Singer = require('./index');
 
 if (process.argv.length < 3
     || process.argv[2] == '-h'
@@ -11,18 +12,23 @@ if (process.argv.length < 3
   return;
 }
 
-var file_path = process.argv[2];
+var songs = []
+  , n = -1
+  , singer = new Singer()
+  , file_path = process.argv[2];
+
+singer.on('end', function () {
+  singNextSong();
+});
 
 fs.stat(file_path, function (err, stats) {
   if (err)
     return printHelp();
 
-  var songs = [];
-
   if (stats.isFile()) {
     if (path.extname(file_path) == '.mp3') {
       songs.push(file_path);
-      singSongs(songs);
+      singSongs();
 
     } else if (path.extname(file_path) == '.list') {
       fs.readFile(file_path, function (err, data) {
@@ -35,7 +41,7 @@ fs.stat(file_path, function (err, stats) {
           if (lines[i])
             songs.push(lines[i]);
         }
-        singSongs(songs);
+        singSongs();
       });
 
     } else {
@@ -52,7 +58,7 @@ fs.stat(file_path, function (err, stats) {
           songs.push(path.join(file_path, files[i]));
         }
       }
-      singSongs(songs);
+      singSongs();
     });
 
   } else {
@@ -66,15 +72,68 @@ function printHelp() {
   console.log('singer musics.list');
 }
 
-function singSongs(songs) {
+function singSongs() {
   if (!songs || songs.length == 0)
     printHelp();
 
-  var singer = new Singer()
-    , i = 0;
+  singNextSong();
 
-  singer.sing(songs[i % songs.length]);
-  singer.on('end', function () {
-    singer.sing(songs[(++i) % songs.length]);
-  });
+  keypress(process.stdin);
+  process.stdin.on('keypress', function (ch, key) {
+    if (!key) return;
+
+    var vol;
+    switch (key.name) {
+      case 'n':
+        singNextSong();
+        break;
+      case 'q':
+        singer.stop();
+        process.stdin.pause();
+        process.exit();
+        break;
+      case 'space':
+        if (singer.state == 'singing') {
+          singer.pause();
+        } else if (singer.state == 'paused') {
+          singer.resume();
+        }
+        break;
+      case 'up':
+        vol = singer.getVolume();
+        if (vol < 100) {
+          vol = parseInt(vol) + 1;
+          singer.turnTo(vol);
+          if (vol % 10 == 0)
+            console.log('Turn up: %d', vol)
+        }
+        break;
+      case 'down':
+        vol = singer.getVolume();
+        if (vol > 0) {
+          vol = parseInt(vol) - 1;
+          singer.turnTo(vol);
+          if (vol % 10 == 0)
+            console.log('Turn down: %d', vol)
+        }
+        break;
+      case 'v':
+        console.log('Current volume: %d', singer.getVolume());
+        break;
+      case 's':
+        console.log('Current song: %s', path.basename(songs[n % songs.length]));
+        break;
+      default:
+        break;
+    }
+
+  }
+  process.stdin.setRawMode(true);
+  process.stdin.resume();
+}
+
+function singNextSong() {
+  var song = songs[(++n) % songs.length];
+  console.log(path.basename(song));
+  singer.sing(song);
 }
